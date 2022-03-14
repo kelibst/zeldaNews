d849996f2d6a210799539db938a7ffc7b519925b
Keli Booster <kbooster17@gmail.com>
2022-03-14 06:59:52 +0000

    update flatlist renderItem

--- screen/renderPost.tsx
+++ screen/renderPost.tsx
@@ -10,7 +10,18 @@ import {
 } from "react-native";
 import React from "react";
 
-const renderPost = (props) => {
+export type Props = {
+  name: string;
+  baseEnthusiasmLevel?: number;
+};
+
+const renderPost: React.FC<Props> = ({ name, baseEnthusiasmLevel }) => {
+  type Tocmp = TouchableOpacity | TouchableNativeFeedback;
+  let Tocmp = TouchableOpacity;
+  if (Platform.OS === "android" && Platform.Version >= 21) {
+    Tocmp = TouchableNativeFeedback;
+  }
+
   return (
     <View>
       <Text>renderPost</Text>
